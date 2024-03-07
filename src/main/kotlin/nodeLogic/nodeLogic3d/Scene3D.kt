0d0a6f344cf3c408b47c018773b4a2d1ab928d93
Storterald <storterald@gmail.com>
2024-03-07 16:52:50 +0100

    added default parameter for shaders

--- src/main/kotlin/nodeLogic/nodeLogic3d/Scene3D.kt
+++ src/main/kotlin/nodeLogic/nodeLogic3d/Scene3D.kt
@@ -3,6 +3,16 @@ package nodeLogic.nodeLogic3d
 import nodeLogic.Scene
 import shaders.Shader
 
-open class Scene3D(initialId: String = "", override val shader: Shader) : Scene(initialId, shader) {
+open class Scene3D(initialId: String = "", override val shader: Shader = Shader("src\\main\\kotlin\\shaders\\VertexShader.glsl", "src\\main\\kotlin\\shaders\\FragmentShader.glsl")) : Scene(initialId, shader) {
+    override fun start() {
+        super.start()
+    }
 
+    override fun update() {
+        super.update()
+    }
+
+    override fun close() {
+        super.close()
+    }
 }
