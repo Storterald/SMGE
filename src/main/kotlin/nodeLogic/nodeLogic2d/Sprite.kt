bf35dd5b36d1aad50e4726ca42f3eb0f6bad1d3f
Storterald <storterald@gmail.com>
2024-03-07 22:49:05 +0100

    the other guy nuked 10 minutes of my time by committing an empty version of this

--- src/main/kotlin/nodeLogic/nodeLogic2d/Sprite.kt
+++ src/main/kotlin/nodeLogic/nodeLogic2d/Sprite.kt
@@ -1,4 +1,19 @@
 package nodeLogic.nodeLogic2d
 
-class Sprite {
+import math.Vec2
+import java.io.File
+
+class Sprite: Object2D {
+    private var file: File
+
+    constructor(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f), file: File): super(id, position, anchorPoint, scale) {
+        this.file = file
+        check(file.exists()) { "The sprite image file doesn't exist." }
+    }
+
+    constructor(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f), filePath: String): super(id, position, anchorPoint, scale) {
+        this.file = File(filePath)
+        check(file.exists()) { "The sprite image file doesn't exist." }
+    }
+
 }
