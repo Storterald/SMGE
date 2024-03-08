7e1781db9334dadad4244922250812ba0f9d84b6
Storterald <storterald@gmail.com>
2024-03-08 17:12:36 +0100

    now allows for negative position values

--- src/main/kotlin/nodeLogic/nodeLogic2d/Node2D.kt
+++ src/main/kotlin/nodeLogic/nodeLogic2d/Node2D.kt
@@ -3,25 +3,14 @@ package nodeLogic.nodeLogic2d
 import nodeLogic.Node
 import math.Vec2
 
-open class Node2D(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f)): Node(id) {
+open class Node2D(id: String = "", open var position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f)): Node(id) {
     init {
-        require(position.x >= 0.0f) { "The x position must be positive" }
-        require(position.y >= 0.0f) { "The y position must be positive" }
-
         require(anchorPoint.x in 0.0f..1.0f) { "The x position must be within 0.0 and 1.0" }
         require(anchorPoint.y in 0.0f..1.0f) { "The y position must be within 0.0 and 1.0" }
 
         require(scale.x >= 0.0f) { "The x scale must be positive" }
         require(scale.y >= 0.0f) { "The y scale must be positive" }
     }
-
-    open var position = position
-        set(value) {
-            require(value.x >= 0.0f) { "The x position must be positive" }
-            require(value.y >= 0.0f) { "The y position must be positive" }
-
-            field = value
-        }
 
     open var anchorPoint = anchorPoint
         set(value) {
--- src/main/kotlin/nodeLogic/nodeLogic3d/Node3D.kt
+++ src/main/kotlin/nodeLogic/nodeLogic3d/Node3D.kt
@@ -3,12 +3,8 @@ package nodeLogic.nodeLogic3d
 import nodeLogic.Node
 import math.Vec3
 
-open class Node3D(id: String = "", position: Vec3 = Vec3(0.0f, 0.0f, 0.0f), anchorPoint: Vec3 = Vec3(0.0f, 0.0f, 0.0f), scale: Vec3 = Vec3(1.0f, 1.0f, 1.0f)): Node(id) {
+open class Node3D(id: String = "", open var position: Vec3 = Vec3(0.0f, 0.0f, 0.0f), anchorPoint: Vec3 = Vec3(0.0f, 0.0f, 0.0f), scale: Vec3 = Vec3(1.0f, 1.0f, 1.0f)): Node(id) {
     init {
-        require(position.x >= 0.0f) { "The x position must be positive" }
-        require(position.y >= 0.0f) { "The y position must be positive" }
-        require(position.z >= 0.0f) { "The y position must be positive" }
-
         require(anchorPoint.x in 0.0f..1.0f) { "The x position must be within 0.0 and 1.0" }
         require(anchorPoint.y in 0.0f..1.0f) { "The y position must be within 0.0 and 1.0" }
         require(anchorPoint.z in 0.0f..1.0f) { "The y position must be within 0.0 and 1.0" }
@@ -17,15 +13,6 @@ open class Node3D(id: String = "", position: Vec3 = Vec3(0.0f, 0.0f, 0.0f), anch
         require(scale.y >= 0.0f) { "The y scale must be positive" }
         require(scale.z >= 0.0f) { "The y scale must be positive" }
     }
-
-    open var position = position
-        set(value) {
-            require(value.x >= 0.0f) { "The x position must be positive" }
-            require(value.y >= 0.0f) { "The y position must be positive" }
-            require(value.z >= 0.0f) { "The y position must be positive" }
-
-            field = value
-        }
 
     open var anchorPoint = anchorPoint
         set(value) {
