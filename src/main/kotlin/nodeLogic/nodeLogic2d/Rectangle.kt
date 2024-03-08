69b92b60c1660f58479d71f83c51631b6cdde523
Storterald <storterald@gmail.com>
2024-03-08 14:38:28 +0100

    added requires and tests

--- src/main/kotlin/nodeLogic/nodeLogic2d/Rectangle.kt
+++ src/main/kotlin/nodeLogic/nodeLogic2d/Rectangle.kt
@@ -8,6 +8,8 @@ data class Rectangle(val x1: Float, val y1: Float, val x2: Float, val y2: Float)
         require(y1 >= 0.0f) { "y1 must be positive." }
         require(x2 >= 0.0f) { "x2 must be positive." }
         require(y2 >= 0.0f) { "y2 must be positive." }
+        require(x2 >= x1) { "x2 must be higher or equal to x1" }
+        require(y2 >= y1) { "y2 must be higher or equal to y1" }
     }
 
     constructor(width: Float, height: Float): this(0.0f, 0.0f, width, height) {
@@ -16,6 +18,8 @@ data class Rectangle(val x1: Float, val y1: Float, val x2: Float, val y2: Float)
     }
 
     constructor(position: Vec2, width: Float, height: Float): this(position.x, position.y, width + position.x, height + position.y) {
+        require(position.x >= 0.0f) { "The x position must be a positive number" }
+        require(position.y >= 0.0f) { "The y position must be a positive number" }
         require(width >= 0.0f) { "The width must be a positive number." }
         require(height >= 0.0f) { "The height must be a positive number" }
     }
---
+++ src/test/kotlin/nodeLogic/nodeLogic2d/RectangleTest.kt
@@ -0,0 +1,103 @@
+package nodeLogic.nodeLogic2d
+
+import math.Vec2
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+
+
+class RectangleTest {
+
+    // ### CONSTRUCTORS TEST ###
+    // ------------------------_
+
+    @Test
+    fun defaultConstructor_createsRectangleWithoutThrowing() {
+        assertDoesNotThrow{ Rectangle(1.0f, 3.0f, 5.0f, 4.0f) }
+    }
+
+    @Test
+    fun defaultConstructor_throwsIfX1ValueIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(-1.0f, 1.0f, 1.0f, 1.0f) }
+    }
+
+    @Test
+    fun defaultConstructor_throwsIfY1ValueIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(1.0f, -1.0f, 1.0f, 1.0f) }
+    }
+
+    @Test
+    fun defaultConstructor_throwsIfX2ValueIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(1.0f, 1.0f, -1.0f, 1.0f) }
+    }
+
+    @Test
+    fun defaultConstructor_throwsIfY2ValueIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(1.0f, 1.0f, 1.0f, -1.0f) }
+    }
+
+    @Test
+    fun defaultConstructor_throwsIfValuesAreNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(-1.0f, -1.0f, -1.0f, -1.0f) }
+    }
+
+    @Test
+    fun defaultConstructor_throwsIfX2IsLowerThanX1() {
+        assertThrows<IllegalArgumentException> { Rectangle(5.0f, 1.0f, 1.0f, 1.0f) }
+    }
+
+    @Test
+    fun defaultConstructor_throwsIfY2IsLowerThanY1() {
+        assertThrows<IllegalArgumentException> { Rectangle(1.0f, 4.0f, 1.0f, 1.0f) }
+    }
+
+    @Test
+    fun secondConstructor_createsRectangleWithoutThrowing() {
+        assertDoesNotThrow { Rectangle(100.0f, 30.0f) }
+    }
+
+    @Test
+    fun secondConstructor_throwsIfWidthIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(-1.0f, 12.0f) }
+    }
+
+    @Test
+    fun secondConstructor_throwsIfHeightIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(134.0f, -12.0f) }
+    }
+
+    @Test
+    fun thirdConstructor_createsRectangleWithoutThrowing() {
+        assertDoesNotThrow { Rectangle(Vec2(20.0f, 10.0f), 100.0f, 30.0f) }
+    }
+
+    @Test
+    fun thirdConstructor_createsRectangleWithCorrectValues() {
+        val rect = Rectangle(Vec2(20.0f, 10.0f), 100.0f, 30.0f)
+        assertEquals(rect.x1, 20.0f)
+        assertEquals(rect.y1, 10.0f)
+        assertEquals(rect.x2, 120.0f)
+        assertEquals(rect.y2, 40.0f)
+    }
+
+    @Test
+    fun thirdConstructor_throwsIfXPositionIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(Vec2(-20.0f, 10.0f), 100.0f, 30.0f) }
+    }
+
+    @Test
+    fun thirdConstructor_throwsIfYPositionIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(Vec2(20.0f, -10.0f), 100.0f, 30.0f) }
+    }
+
+    @Test
+    fun thirdConstructor_throwsIfWidthIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(Vec2(20.0f, 10.0f), -100.0f, 30.0f) }
+    }
+
+    @Test
+    fun thirdConstructor_throwsIfHeightIsNegative() {
+        assertThrows<IllegalArgumentException> { Rectangle(Vec2(20.0f, 10.0f), 100.0f, -30.0f) }
+    }
+
+}
