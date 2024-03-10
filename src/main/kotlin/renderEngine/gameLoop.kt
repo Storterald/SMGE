dde4480a95b4a28bbac6fc6e9eae1ab0b774e347
Storterald <storterald@gmail.com>
2024-03-10 17:51:37 +0100

    now uses global variable 'WINDOW' and Int.MAX_VALUE instead of 2147483647

--- src/main/kotlin/renderEngine/gameLoop.kt
+++ src/main/kotlin/renderEngine/gameLoop.kt
@@ -3,12 +3,15 @@ package renderEngine
 import org.lwjgl.glfw.GLFW.glfwGetTime
 import org.lwjgl.glfw.GLFW.glfwWindowShouldClose
 
-fun gameLoop(window: Long, fpsCap: Int = 0, codeBlock: () -> Unit) {
+import WINDOW
+
+fun gameLoop(fpsCap: Int = 0, codeBlock: () -> Unit) {
+    check(WINDOW != 0L) { "Create the display before using gameLoop()." }
     require(fpsCap >= 0)
-    val FPS_CAP = if (fpsCap == 0) 2147483647 else fpsCap
+    val FPS_CAP = if (fpsCap == 0) Int.MAX_VALUE else fpsCap
 
     var lastTime: Double = glfwGetTime()
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(WINDOW)) {
         codeBlock()
 
         while (glfwGetTime() < lastTime + 1.0/FPS_CAP) {
