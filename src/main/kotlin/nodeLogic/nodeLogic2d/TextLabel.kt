7c2355e8390ebbe88671f137a0b360591a2464b5
Storterald <storterald@gmail.com>
2024-03-10 18:27:26 +0100

    Created TextLabel

---
+++ src/main/kotlin/nodeLogic/nodeLogic2d/TextLabel.kt
@@ -0,0 +1,126 @@
+package nodeLogic.nodeLogic2d
+
+import math.Vec2
+import java.awt.Font
+import java.awt.RenderingHints
+import java.awt.image.BufferedImage
+import java.io.File
+
+class TextLabel: Object2D {
+    constructor(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f), text: String, font: Font = Font("Arial", Font.PLAIN, 12)): super(id, position, anchorPoint, scale) {
+        this.font = font
+        this.text = text
+
+        this.scale = scale
+        this.anchorPoint = anchorPoint
+    }
+
+    constructor(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f), fontSize: Float, text: String, font: Font = Font("Arial", Font.PLAIN, 20)): super(id, position, anchorPoint, scale) {
+        require(fontSize > 0.0f) { "The font size must be higher than 0." }
+
+        this.font = font.deriveFont(font.style, fontSize)
+        this.text = text
+        this.fontSize = fontSize
+
+        this.scale = scale
+        this.anchorPoint = anchorPoint
+    }
+
+    constructor(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f), text: String, fontFile: File): super(id, position, anchorPoint, scale) {
+        check(fontFile.exists()) { "The font file does not exist." }
+        require(fontFile.extension in arrayOf("otf", "ttf", "woff", "svg", "eof", "OTF", "TTF", "WOFF", "SVG", "EOF")) { "The font must be a otf, ttf, woff, svg or eof." }
+
+        val tempFont = Font.createFont(Font.TRUETYPE_FONT, fontFile)
+        this.font = tempFont.deriveFont(tempFont.style, fontSize)
+        this.text = text
+
+        this.scale = scale
+        this.anchorPoint = anchorPoint
+    }
+
+    constructor(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f), fontSize: Float, text: String, fontFile: File): super(id, position, anchorPoint, scale) {
+        require(fontSize > 0.0f) { "The font size must be higher than 0." }
+        check(fontFile.exists()) { "The font file does not exist." }
+        require(fontFile.extension in arrayOf("otf", "ttf", "woff", "svg", "eof", "OTF", "TTF", "WOFF", "SVG", "EOF")) { "The font must be a otf, ttf, woff, svg or eof." }
+
+        val tempFont = Font.createFont(Font.TRUETYPE_FONT, fontFile)
+        this.font = tempFont.deriveFont(tempFont.style, fontSize)
+        this.text = text
+        this.fontSize = fontSize
+
+        this.scale = scale
+        this.anchorPoint = anchorPoint
+    }
+
+    constructor(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f), text: String, fontPath: String): super(id, position, anchorPoint, scale) {
+        val fontFile = File(fontPath)
+        check(fontFile.exists()) { "The font file does not exist." }
+        require(fontFile.extension in arrayOf("otf", "ttf", "woff", "svg", "eof", "OTF", "TTF", "WOFF", "SVG", "EOF")) { "The font must be a otf, ttf, woff, svg or eof." }
+
+        val tempFont: Font = Font.createFont(Font.TRUETYPE_FONT, fontFile)
+        this.font = tempFont.deriveFont(tempFont.style, fontSize)
+        this.text = text
+
+        this.scale = scale
+        this.anchorPoint = anchorPoint
+    }
+
+    constructor(id: String = "", position: Vec2 = Vec2(0.0f, 0.0f), anchorPoint: Vec2 = Vec2(0.0f, 0.0f), scale: Vec2 = Vec2(1.0f, 1.0f), fontSize: Float, text: String, fontPath: String): super(id, position, anchorPoint, scale) {
+        require(fontSize > 0.0f) { "The font size must be higher than 0." }
+
+        val fontFile = File(fontPath)
+        check(fontFile.exists()) { "The font file does not exist." }
+        require(fontFile.extension in arrayOf("otf", "ttf", "woff", "svg", "eof", "OTF", "TTF", "WOFF", "SVG", "EOF")) { "The font must be a otf, ttf, woff, svg or eof." }
+
+        val tempFont: Font = Font.createFont(Font.TRUETYPE_FONT, fontFile)
+        this.font = tempFont.deriveFont(tempFont.style, fontSize)
+        this.text = text
+        this.fontSize = fontSize
+
+        this.scale = scale
+        this.anchorPoint = anchorPoint
+    }
+
+    var text: String = ""
+        set(value) {
+            createImage(value, font)
+
+            field = value
+        }
+
+    var font: Font = Font.createFont(Font.TRUETYPE_FONT, File("src\\main\\resources\\arial.ttf"))
+        set(value) {
+            createImage(text, value)
+
+            field = value
+        }
+
+    var fontSize: Float = 12.0f
+        set(value) {
+            require(value > 0.0f) { "The font size must be higher than 0." }
+            font = font.deriveFont(font.style, value)
+
+            field = value
+        }
+
+    lateinit var image: BufferedImage
+        private set
+
+    private fun createImage(text: String, font: Font) {
+        image = BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB)
+        var graphics2D = image.createGraphics()
+        graphics2D.font = font
+        var fontMetrics = graphics2D.fontMetrics
+        size = Vec2(fontMetrics.stringWidth(text).toFloat(), fontMetrics.height.toFloat())
+        graphics2D.dispose()
+
+        image = BufferedImage(if (size.x.toInt() > 0) size.x.toInt() else 1, if (size.y.toInt() > 0) size.y.toInt() else 1, BufferedImage.TYPE_INT_ARGB)
+        graphics2D = image.createGraphics()
+        graphics2D.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY)
+        graphics2D.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY)
+        graphics2D.font = font
+        fontMetrics = graphics2D.fontMetrics
+        graphics2D.drawString(text, 0, fontMetrics.ascent)
+        graphics2D.dispose()
+    }
+}
---
+++ src/test/kotlin/nodeLogic/nodeLogic2d/TextLabelTest.kt
@@ -0,0 +1,121 @@
+package nodeLogic.nodeLogic2d
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
+import java.awt.Font
+import java.io.File
+
+class TextLabelTest {
+
+    // ### CONSTRUCTORS TEST ###
+    // -------------------------
+
+    @Test
+    fun firstConstructor_createsTextLabelWithoutThrowing() {
+        assertDoesNotThrow { TextLabel(text = "Test") }
+    }
+
+    @Test
+    fun firstConstructor_getsImageSize() {
+        val textLabel = TextLabel(text = "Test")
+        assertEquals(24.0f, textLabel.size.x)
+        assertEquals(14.0f, textLabel.size.y)
+    }
+
+    @Test
+    fun secondConstructor_createsTextLabelWithoutThrowing() {
+        assertDoesNotThrow { TextLabel(fontSize = 35.0f, text = "Test") }
+    }
+
+    @Test
+    fun secondConstructor_getsImageSize() {
+        val textLabel = TextLabel(fontSize = 35.0f, text = "Test")
+        assertEquals(67.0f, textLabel.size.x)
+        assertEquals(41.0f, textLabel.size.y)
+    }
+
+    @Test
+    fun thirdConstructor_createsTextLabelWithoutThrowing() {
+        assertDoesNotThrow { TextLabel(text = "Test", fontFile = File("src\\test\\resources\\testFont.otf")) }
+    }
+
+    @Test
+    fun thirdConstructor_getsImageSize() {
+        val textLabel = TextLabel(text = "Test", fontFile = File("src\\test\\resources\\testFont.otf"))
+        assertEquals(26.0f, textLabel.size.x)
+        assertEquals(15.0f, textLabel.size.y)
+    }
+
+    @Test
+    fun fourthConstructor_createsTextLabelWithoutThrowing() {
+        assertDoesNotThrow { TextLabel(fontSize = 35.0f, text = "Test", fontFile = File("src\\test\\resources\\testFont.otf")) }
+    }
+
+    @Test
+    fun fourthConstructor_getsImageSize() {
+        val textLabel = TextLabel(fontSize = 35.0f, text = "Test", fontFile = File("src\\test\\resources\\testFont.otf"))
+        assertEquals(77.0f, textLabel.size.x)
+        assertEquals(42.0f, textLabel.size.y)
+    }
+
+    @Test
+    fun fifthConstructor_createsTextLabelWithoutThrowing() {
+        assertDoesNotThrow { TextLabel(text = "Test", fontPath = "src\\test\\resources\\testFont.otf") }
+    }
+
+    @Test
+    fun fifthConstructor_getsImageSize() {
+        val textLabel = TextLabel(text = "Test", fontPath = "src\\test\\resources\\testFont.otf")
+        assertEquals(26.0f, textLabel.size.x)
+        assertEquals(15.0f, textLabel.size.y)
+    }
+
+    @Test
+    fun sixthConstructor_createsTextLabelAndWithoutThrowing() {
+        assertDoesNotThrow { TextLabel(fontSize = 35.0f, text = "Test", fontPath = "src\\test\\resources\\testFont.otf") }
+    }
+
+    @Test
+    fun sixthConstructor_getsImageSize() {
+        val textLabel = TextLabel(fontSize = 35.0f, text = "Test", fontPath = "src\\test\\resources\\testFont.otf")
+        assertEquals(77.0f, textLabel.size.x)
+        assertEquals(42.0f, textLabel.size.y)
+    }
+
+    // ### SETTERS TEST ###
+    // --------------------
+
+    @Test
+    fun text_createsTheImageAndChangesTheSize() {
+        val textLabel = TextLabel(text = "Test", fontPath = "src\\test\\resources\\testFont.otf")
+        textLabel.text = "New text"
+
+        assertEquals(50.0f, textLabel.size.x)
+        assertEquals(15.0f, textLabel.size.y)
+    }
+
+    @Test
+    fun font_createsTheImageAndChangesTheSize() {
+        val textLabel = TextLabel(text = "Test", fontPath = "src\\test\\resources\\testFont.otf")
+        textLabel.font = Font("Arial", Font.PLAIN, 12)
+
+        assertEquals(24.0f, textLabel.size.x)
+        assertEquals(14.0f, textLabel.size.y)
+    }
+
+    @Test
+    fun fontSize_createsTheImageAndChangesTheSize() {
+        val textLabel = TextLabel(text = "Test", fontPath = "src\\test\\resources\\testFont.otf")
+        textLabel.fontSize = 35.0f
+
+        assertEquals(77.0f, textLabel.size.x)
+        assertEquals(42.0f, textLabel.size.y)
+    }
+
+    @Test
+    fun fontSize_throwsIfValueIsNegative() {
+        val textLabel = TextLabel(text = "Test", fontPath = "src\\test\\resources\\testFont.otf")
+        assertThrows<IllegalArgumentException> { textLabel.fontSize = -1.0f }
+    }
+}
